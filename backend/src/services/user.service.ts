0cd1cfb3dae45de0fc6725122f62c9a313e23dca
Saad Sayyed <saadsyed950@gmail.com>
2026-04-24 18:21:01 +0530

    Sync user service

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,1 +1,32 @@
 // backend/src/services/user.service.ts
+
+// Import types
+import { type SyncUser } from "../types/user.type";
+
+// Import ORM instance
+import prisma from "../lib/prisma.orm";
+
+// Service to sync user from clerk
+export const syncUserService = async (data: SyncUser) => {
+  const existing = await prisma.users.findUnique({
+    where: {
+      clerkId: data.clerkId,
+    },
+  });
+
+  // If user exists do not sync
+  if (existing) {
+    throw new Error("User already exists");
+  }
+
+  // Sync clerk's user to database
+  return await prisma.users.create({
+    data: {
+      clerkId: data.clerkId,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      profilePicture: data.profilePicture,
+    },
+  });
+};
